33b47a7caf981794c92d9ff3b6c1d36e69a4da8e
Brian Bolli <brian.bolli@arctg.com>
2015-01-15 15:00:41 -0600

    installed Kunena for testing purposes

---
+++ logs/error.php
@@ -0,0 +1,10 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2014-12-30 21:09:10 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2014-12-30T21:09:10+00:00	INFO 127.0.0.1	joomlafailure	Username and password do not match or you do not have an account yet.
+2014-12-30T21:09:14+00:00	INFO 127.0.0.1	joomlafailure	Username and password do not match or you do not have an account yet.
+2014-12-30T21:09:16+00:00	INFO 127.0.0.1	joomlafailure	Username and password do not match or you do not have an account yet.
+2014-12-30T21:09:25+00:00	INFO 127.0.0.1	joomlafailure	Username and password do not match or you do not have an account yet.
---
+++ logs/upload.error.php
@@ -0,0 +1,13 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2014-10-14 19:58:36 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2014-10-14T19:58:36+00:00	INFO 127.0.0.1	upload	Error on upload: 
+2014-10-14T19:59:51+00:00	INFO 127.0.0.1	upload	Error on upload: 
+2014-10-14T20:06:36+00:00	INFO 127.0.0.1	upload	Invalid: C:\Users\Brian Bolli\git\joomla-cms\images\: 
+2014-10-14T20:17:51+00:00	INFO 127.0.0.1	upload	Invalid: C:\Users\Brian Bolli\git\joomla-cms\images\: 
+2014-10-14T20:35:47+00:00	INFO 127.0.0.1	upload	Invalid: C:\Users\Brian Bolli\git\joomla-cms\images\: 
+2014-10-14T20:36:59+00:00	INFO 127.0.0.1	upload	Invalid: C:\Users\Brian Bolli\git\joomla-cms\images\: 
+2014-10-14T20:37:25+00:00	INFO 127.0.0.1	upload	Invalid: C:\Users\Brian Bolli\git\joomla-cms\images\: 
